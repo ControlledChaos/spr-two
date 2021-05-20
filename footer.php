c1e38e09f4836541720a35b2bcbd2037466e0b2e
ControlledChaos <greg@ccdzine.com>
2021-05-20 15:29:02 -0700

    Smooth scroll, anchor links, scroll to top.

--- footer.php
+++ footer.php
@@ -17,6 +17,7 @@ use SPR_Two\Classes\Front as Front;
 	<?php Front\tags()->before_footer(); ?>
 	<?php Front\tags()->footer(); ?>
 	<?php Front\tags()->after_footer(); ?>
+	<button onclick="topFunction()" id="back-to-top" title="Go to top"><svg role="presentation" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M4.465 366.475l7.07 7.071c4.686 4.686 12.284 4.686 16.971 0L224 178.053l195.494 195.493c4.686 4.686 12.284 4.686 16.971 0l7.07-7.071c4.686-4.686 4.686-12.284 0-16.97l-211.05-211.051c-4.686-4.686-12.284-4.686-16.971 0L4.465 349.505c-4.687 4.686-4.687 12.284 0 16.97z"/></svg><span class="screen-reader-text"><?php _e( 'Top', 'spr-two' ); ?></span></button>
 
 </div><!-- #page -->
 
@@ -44,6 +45,72 @@ document.addEventListener( 'wpcf7mailsent', function( event ) {
     location = 'http://sequoiapacificrealty.com/application-deposit/';
 }, false );
 </script>
+<?php if ( is_front_page() ) : ?>
+<script>
+jQuery(document).ready(function($) {
+	var offset = -180;
+	$('a[href*="#"]')
+	// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function(event) {
+		// On-page links
+		if (
+			location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+			&&
+			location.hostname == this.hostname
+			) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$('html, body').animate({
+				scrollTop: target.offset().top + offset
+				}, 500, function() {
+					// Callback after animation
+					// Must change focus!
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) { // Checking if the target was focused
+						return false;
+					} else {
+						$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+						$target.focus(); // Set focus again
+					};
+				});
+			}
+		}
+	});
+});
+</script>
+<?php endif; ?>
+<script>
+jQuery(document).ready( function($) {
+	$(window).scroll( function() {
+		if ( $(this).scrollTop() > 640 ) {
+			$( '#back-to-top' ).fadeIn( 350 );
+		} else {
+			$( '#back-to-top' ).fadeOut( 350 );
+		}
+	});
+
+	$( '#back-to-top' ).click( function() {
+		$( 'html, body' ).animate( { scrollTop : 0 }, 500 );
+	});
+});
+</script>
+<style>
+#back-to-top {
+	display: none;
+	position: fixed;
+	right: 2rem;
+	bottom: 2rem;
+	transition: none;
+}
+</style>
 
 </body>
 </html>
