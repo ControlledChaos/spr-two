cd547477f65d3891d01e7799a81195820d386e9e
ControlledChaos <greg@ccdzine.com>
2021-05-27 12:32:32 -0700

    Toolbar: customize link.

--- template-parts/header/toolbar.php
+++ template-parts/header/toolbar.php
@@ -41,6 +41,8 @@ $log_link = sprintf(
 
 $dashboard_icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M288 32C128.94 32 0 160.94 0 320c0 52.8 14.25 102.26 39.06 144.8 5.61 9.62 16.3 15.2 27.44 15.2h443c11.14 0 21.83-5.58 27.44-15.2C561.75 422.26 576 372.8 576 320c0-159.06-128.94-288-288-288zm102.77 119.59l-61.33 184C343.13 347.33 352 364.54 352 384c0 11.72-3.38 22.55-8.88 32H232.88c-5.5-9.45-8.88-20.28-8.88-32 0-33.94 26.5-61.43 59.9-63.59l61.34-184.01c4.17-12.56 17.73-19.45 30.36-15.17 12.57 4.19 19.35 17.79 15.17 30.36z"/></svg>';
 
+$palette_icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M204.3 5C104.9 24.4 24.8 104.3 5.2 203.4c-37 187 131.7 326.4 258.8 306.7 41.2-6.4 61.4-54.6 42.5-91.7-23.1-45.4 9.9-98.4 60.9-98.4h79.7c35.8 0 64.8-29.6 64.9-65.3C511.5 97.1 368.1-26.9 204.3 5zM96 320c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm32-128c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128-64c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128 64c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"/></svg>';
+
 global $post;
 $post_id  = get_post_type( $post->ID );
 $get_type = get_post_type_object( $post_id );
@@ -64,6 +66,13 @@ if ( $contact_page ) {
 			<ul>
 				<?php
 
+				// Edit link.
+				if ( is_singular() && is_user_logged_in() && current_user_can( 'edit_posts' ) ) : ?>
+					<li><a href="<?php echo esc_url( get_edit_post_link() ); ?>" title="<?php _e( 'Edit this page', 'spr-two' ); ?>"><span class="toolbar-icon toolbar-dashboard-icon" role="presentation"><?php echo $edit_icon; ?></span> <span class="toolbar-link-text"><?php echo __( 'Edit', 'spr-two' ) . ' ' . $type; ?></span></a></li>
+				<?php
+
+				endif;
+
 				// Dashboard link.
 				if ( is_user_logged_in() && current_user_can( 'edit_posts' ) ) :
 
@@ -74,9 +83,11 @@ if ( $contact_page ) {
 
 				endif;
 
-				// Edit link.
-				if ( is_singular() && is_user_logged_in() && current_user_can( 'edit_posts' ) ) : ?>
-				<li><a href="<?php echo esc_url( get_edit_post_link() ); ?>" title="<?php _e( 'Edit this page', 'spr-two' ); ?>"><span class="toolbar-icon toolbar-dashboard-icon" role="presentation"><?php echo $edit_icon; ?></span> <span class="toolbar-link-text"><?php echo __( 'Edit', 'spr-two' ) . ' ' . $type; ?></span></a></li>
+				if ( current_user_can( 'customize' ) ) :
+
+				?>
+				<li><a href="<?php echo esc_url( admin_url( 'customize.php' ) ); ?>" title="<?php _e( 'Customize this site', 'spr-two' ); ?>"><span class="toolbar-icon toolbar-customizer-icon" role="presentation"><?php echo $palette_icon; ?></span> <span class="toolbar-link-text"><?php _e( 'Customize', 'spr-two' ); ?></span></a></li>
+
 				<?php
 
 				endif;
