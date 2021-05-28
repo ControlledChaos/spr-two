3d250e36b7a627c46367781341356366d878b152
ControlledChaos <greg@ccdzine.com>
2021-05-28 01:38:08 -0700

    No-content file for ACF.

---
+++ template-parts/content/content-none-acf.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @package    SPR_Two
+ * @subpackage Templates
+ * @category   Content
+ * @since      1.0.0
+ */
+
+namespace SPR_Two;
+
+// Alias namespaces.
+use SPR_Two\Classes\Front as Front;
+
+?>
+<div class="no-results not-found">
+
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'spr-two' ); ?></h1>
+	</header>
+
+	<div class="page-content" itemprop="articleBody">
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) :
+
+			printf(
+				'<p>' . wp_kses(
+					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'spr-two' ),
+					[
+						'a' => [
+							'href' => [],
+						],
+					]
+				) . '</p>',
+				esc_url( admin_url( 'post-new.php' ) )
+			);
+
+		elseif ( is_search() ) : ?>
+
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'spr-two' ); ?></p>
+			<?php
+			get_search_form();
+
+		else : ?>
+
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'spr-two' ); ?></p>
+			<?php
+			get_search_form();
+
+		endif; ?>
+	</div>
+
+</div>
