6b7d8d10209affe72a24c42afb3fd587a0a576db
ControlledChaos <greg@ccdzine.com>
2021-05-28 18:28:52 -0700

    Page template for ACF.

---
+++ template-parts/content/content-page-acf.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @package    SPR_Two
+ * @subpackage Templates
+ * @category   Content
+ * @since      1.0.0
+ */
+
+namespace SPR_Two;
+
+// Alias namespaces.
+use SPR_Two\Classes\Front as Front;
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
+
+	<header class="entry-header">
+		<?php
+		if ( is_front_page() ) {
+
+			// Front page uses h1 in the page header so h2 here.
+			the_title( '<h2 class="entry-title">', '</h2>' );
+		} else {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} ?>
+	</header>
+
+	<?php if ( ! is_front_page() ) {
+		Front\tags()->post_thumbnail();
+	} ?>
+
+	<div class="entry-content" itemprop="articleBody">
+		<?php
+		the_content();
+
+		wp_link_pages( [
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'spr-two' ),
+			'after'  => '</div>',
+		] );
+		?>
+	</div>
+</article>
