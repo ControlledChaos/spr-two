3d8a4d8ea9af67cccbe17ba564d809667e1a9159
ControlledChaos <greg@ccdzine.com>
2021-05-31 11:35:17 -0700

    Add content templates for location post type.

---
+++ template-parts/content/content-location.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @package    SPR_Two
+ * @subpackage Templates
+ * @category   Content
+ * @since      1.0.0
+ */
+
+namespace SPR_Two;
+
+// Alias namespaces.
+use SPR_Two\Classes\Front as Front;
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header>
+
+	<div class="entry-content" itemprop="articleBody">
+		<?php
+		the_content();
+
+		wp_link_pages( [
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'spr-two' ),
+			'after'  => '</div>',
+		] );
+		?>
+	</div>
+</article>
