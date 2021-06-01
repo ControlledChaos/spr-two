ecff4ea1aa9acf26cd865a67c84fc45d38c388ea
ControlledChaos <greg@ccdzine.com>
2021-06-01 13:57:07 -0700

    No-content templates.

--- template-parts/content/content-none-acf.php
+++ template-parts/content/content-none-acf.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying a message that posts cannot be found
+ * ACF template part for displaying a message that posts cannot be found
  *
  * @package    SPR_Two
  * @subpackage Templates
@@ -11,43 +11,48 @@
 namespace SPR_Two;
 
 // Alias namespaces.
-use SPR_Two\Classes\Front as Front;
+use SPR_Two\Classes\Front  as Front,
+	SPR_Two\Classes\Vendor as Vendor;
+
+// Not found message.
+if ( is_search() ) {
+	$message = __( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'spr-two' );
+} else {
+	$message = __( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'spr-two' );
+}
+
+// Search form from the MLS plugin.
+$mls_search = '[idx_search title="" link="1lkpig7k5t4z" buttontext="Find Now" detailed_search="on" destination="local" user_sorting="off" location_search="on" property_type_enabled="off" property_type="A,B,D" std_fields="beds,baths,square_footage,list_price" theme="hori_square_light" orientation="horizontal" title_font="" border_style="squared" widget_drop_shadow="on" background_color="#222222" title_text_color="#ffffff" field_text_color="#ffffff" detailed_search_text_color="#ffffff" submit_button_shine="shine" submit_button_background="#222222" submit_button_text_color="#ffffff" allow_sold_searching="off" default_view="list" listings_per_page="10" allow_pending_searching="off"]';
 
 ?>
 <div class="no-results not-found">
 
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'spr-two' ); ?></h1>
+		<h1 class="entry-title"><?php esc_html_e( 'Nothing Found', 'spr-two' ); ?></h1>
 	</header>
 
 	<div class="page-content" itemprop="articleBody">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
-			printf(
-				'<p>' . wp_kses(
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'spr-two' ),
-					[
-						'a' => [
-							'href' => [],
-						],
-					]
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
+		<p><?php echo $message; ?></p>
 
-		elseif ( is_search() ) : ?>
+		<?php
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'spr-two' ); ?></p>
-			<?php
+		if ( Vendor\mls()->flexmls_idx() ) :
+
+		?>
+		<div class="mls-search-form mls-search-page">
+
+			<h2><?php _e( 'Property Search', 'spr-two' ); ?></h2>
+			<p><?php _e( 'Would you like to search available properties?', 'spr-two' ); ?></p>
+
+			<?php echo do_shortcode( $mls_search ); ?>
+		</div>
+		<?php
+
+		else :
 			get_search_form();
+		endif;
 
-		else : ?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'spr-two' ); ?></p>
-			<?php
-			get_search_form();
-
-		endif; ?>
+		?>
 	</div>
-
 </div>
--- template-parts/content/content-none.php
+++ template-parts/content/content-none.php
@@ -11,43 +11,48 @@
 namespace SPR_Two;
 
 // Alias namespaces.
-use SPR_Two\Classes\Front as Front;
+use SPR_Two\Classes\Front  as Front,
+	SPR_Two\Classes\Vendor as Vendor;
+
+// Not found message.
+if ( is_search() ) {
+	$message = __( 'Sorry, but nothing matched your search terms.', 'spr-two' );
+} else {
+	$message = __( 'It seems we can&rsquo;t find what you&rsquo;re looking for.', 'spr-two' );
+}
+
+// Search form from the MLS plugin.
+$mls_search = '[]';
 
 ?>
 <div class="no-results not-found">
 
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'spr-two' ); ?></h1>
+		<h1 class="entry-title"><?php esc_html_e( 'Nothing Found', 'spr-two' ); ?></h1>
 	</header>
 
 	<div class="page-content" itemprop="articleBody">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
-			printf(
-				'<p>' . wp_kses(
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'spr-two' ),
-					[
-						'a' => [
-							'href' => [],
-						],
-					]
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
+		<p><?php echo $message; ?></p>
 
-		elseif ( is_search() ) : ?>
+		<?php
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'spr-two' ); ?></p>
-			<?php
+		if ( Vendor\mls()->flexmls_idx() ) :
+
+		?>
+		<div class="mls-search-form mls-search-page">
+
+			<h2><?php _e( 'Property Search', 'spr-two' ); ?></h2>
+			<p><?php _e( 'Would you like to search available properties?', 'spr-two' ); ?></p>
+
+			<?php echo do_shortcode( $mls_search ); ?>
+		</div>
+		<?php
+
+		else :
 			get_search_form();
+		endif;
 
-		else : ?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'spr-two' ); ?></p>
-			<?php
-			get_search_form();
-
-		endif; ?>
+		?>
 	</div>
-
 </div>
