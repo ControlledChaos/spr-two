efc8a0cb36e084e7b9e903595382eb845df196ca
ControlledChaos <greg@ccdzine.com>
2021-06-10 17:26:45 -0700

    Add email links to front page staff entries.

--- template-parts/content/partials/front-acf-staff.php
+++ template-parts/content/partials/front-acf-staff.php
@@ -16,6 +16,8 @@ use SPR_Two\Classes\Front as Front;
 // Get ACF fields.
 $staff_members = get_sub_field( 'spr_front_staff_members' );
 
+$mail_icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"/></svg>';
+
 if ( $staff_members ) :
 
 ?>
@@ -49,9 +51,17 @@ if ( $staff_members ) :
 		<li style="display: inline-block;">
 			<div class="front-staff-slide-wrap">
 				<figure>
-					<img class="front-staff-avatar avatar" src="<?php echo $src; ?>" alt="<?php esc_html_e( "$name's profile pic" ); ?>" width="240" height="240" />
+					<a href="<?php the_permalink( $staff_member->ID ); ?>">
+						<img class="front-staff-avatar avatar" src="<?php echo $src; ?>" alt="<?php esc_html_e( "$name's profile pic" ); ?>" width="240" height="240" />
+					</a>
 					<figcaption>
-						<h4><?php echo $name; ?></h4>
+						<h4><?php echo $name; ?>
+							<?php if ( $email ) : ?>
+							<a class="staff-email-icon-link" href="mailto:<?php echo $email; ?>">
+								<?php echo $mail_icon; ?><span class="screen-reader-text"> <?php echo $email; ?></span>
+							</a>
+							<?php endif; ?>
+						</h4>
 						<p><?php echo $position; ?></p>
 					</figcaption>
 				</figure>
@@ -59,7 +69,13 @@ if ( $staff_members ) :
 			</div>
 			<div id="<?php echo "{$staff_member->ID}-bio-modal"; ?>" class="front-staff-modal" style="display: none;">
 				<img class="alignleft avatar" src="<?php echo $src; ?>" alt="<?php esc_html_e( "$name's profile pic" ); ?>" width="120" height="120" />
-				<p class="modal-heading"><?php echo $name; ?></p>
+				<p class="modal-heading"><?php echo $name; ?>
+					<?php if ( $email ) : ?>
+					<a class="staff-email-icon-link" href="mailto:<?php echo $email; ?>">
+						<?php echo $mail_icon; ?><span class="screen-reader-text"> <?php echo $email; ?></span>
+					</a>
+					<?php endif; ?>
+				</p>
 				<p><?php echo $position; ?></p>
 				<div class="bio">
 					<?php echo $bio; ?>
